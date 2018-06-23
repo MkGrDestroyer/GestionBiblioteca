9dc62919fed19a2e918a2bb76667a5f5e2fd5190
Suigeneris <miguel15081998@protonmail.com>
2018-06-22 21:29:47 -0500

    avance de capa datos 3

--- GestionA/Datos/dMaterial.cs
+++ GestionA/Datos/dMaterial.cs
@@ -12,8 +12,39 @@ namespace Datos
         Database db = new Database();
         public string Insertar(eMaterial objmaterial)
         {
-            SqlConnection con = db.ConectaDb();
-            string insert = string.Format("INSERT INTO Material ")
+            try
+            {
+                SqlConnection con = db.ConectaDb();
+                string insert = string.Format("INSERT INTO Material (titulo, autor, tema, aniopublicacion, editorial_productor, estado, inventario, articulosprestados, tipomaterial) VALUES " +
+                    "('{0}', '{1}', '{2}', {3}, '{4}', '{5}', {6}, {7}, '{8}' )", objmaterial.titulo, objmaterial.autor);
+                SqlCommand cmd = new SqlCommand(insert, con);
+                cmd.ExecuteNonQuery();
+                return "inserto";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
+        }
+        public string Modificar(eMaterial objmaterial)
+        {
+            try
+            {
+                SqlConnection con = db.ConectaDb();
+                string update = string.Format("UPDATE Material ")
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
         }
     }
 }
